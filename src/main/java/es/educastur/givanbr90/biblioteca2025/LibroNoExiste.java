5be0010f290b8d06607d191f12cb5706656d3320
1dawd17 <1dawd17@A204PC17.desarrolloweb.local>
2026-01-13 10:07:52 +0100

    Biblioteca a 13/01/2026 LibroNoExiste

---
+++ src/main/java/es/educastur/givanbr90/biblioteca2025/LibroNoExiste.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.educastur.givanbr90.biblioteca2025;
+
+/**
+ *
+ * @author 1dawd17
+ */
+public class LibroNoExiste extends Exception {
+    public LibroNoExiste (String cadena){
+        super(cadena); 
+    }
+    
+}
