f6bc2bbe838675ead422d7aac8b135222f9dbeaf
1dawd17 <1dawd17@A204PC17.desarrolloweb.local>
2026-01-13 10:08:21 +0100

    Biblioteca a 13/01/2026 UsuariosSancionados

---
+++ src/main/java/es/educastur/givanbr90/biblioteca2025/UsuariosSancionados.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.educastur.givanbr90.biblioteca2025;
+
+/**
+ *
+ * @author 1dawd17
+ */
+public class UsuariosSancionados extends Exception {
+    public UsuariosSancionados (String cadena){
+        super(cadena); 
+    }
+}
+    
+
